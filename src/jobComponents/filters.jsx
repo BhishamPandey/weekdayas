e7278730e049472f3593c0b58f3cb05ca7bef28c
BhishamPandey <pandeybhishm2@gmail.com>
2024-05-05 23:31:01 +0530

    ..

--- src/jobComponents/filters.jsx
+++ src/jobComponents/filters.jsx
@@ -4,26 +4,26 @@ import { Box, Container, TextField } from '@mui/material';
 import FilterInput from '../shared/filtersUi/filterInput';
 import FilterSelector from '../shared/filtersUi/filterSelector'
 const Filters = ({ filters, setFilters }) => {
-  // Define available filters here
+  
   const filterOptions = [
-     // Predefined filter options for job listings.
+     
     { key: 'location', name: 'Location' },
     { key: 'jobRole', name: 'Job Role' },
     { key: 'minExp', name: 'Min Experience' },
     {key:'companyName', name:'Company Name'},
     { key: 'minJdSalary', name: 'Min base Pay' },
     { key: 'workType', name: 'Work Type' }
-    // Add more filters as needed
+    
   ];
 
-  // State to track the currently selected filter
+  
   const [selectedFilter, setSelectedFilter] = useState('');
 
   const handleFilterTypeChange = (event) => {
     const newFilterType = event.target.value;
     setSelectedFilter(newFilterType);
 
-    // Reset the corresponding filter value when changing the filter type
+    
     setFilters({
       ...filters,
       [newFilterType]: ''
@@ -41,10 +41,10 @@ const Filters = ({ filters, setFilters }) => {
   };
 
   const handleWorkTypeChange = (event) => {
-    // Update workType filter directly
+    
     setFilters({
       ...filters,
-      [selectedFilter]: event.target.value, // Directly updates the workType in the filters state.
+      [selectedFilter]: event.target.value, 
     });
   };
 
@@ -52,19 +52,19 @@ const Filters = ({ filters, setFilters }) => {
     <Container  display='flex' justifyContent='center' sx={{width:'33%'}}>
     <Box sx={{  }}>
       <Box sx={{ mb: 2}}>
-{/*         filter selector contains all  the filter options */}
+
         <FilterSelector selectedFilter={selectedFilter} handleFilterTypeChange={handleFilterTypeChange} filterOptions={filterOptions}/>
       </Box>
-{/*       for work type we need radio butons so different rendering */}
+
       {selectedFilter === 'workType' ? (
         <FilterInput selectedFilter={selectedFilter} filters={filters} handleFilterValueChange={handleFilterTypeChange} handleWorkTypeChange={handleWorkTypeChange} filterOptions={filterOptions}/>
       ) : (
-        // adding input textfield where we can input our filtering requirements
+        
         <Box sx={{ mb: 2 }}>
           <TextField
             fullWidth
-            type="text"
-            name={selectedFilter} //this is used whenever the value changes to update the jobLists accordingly
+            type="text" 
+            name={selectedFilter}
             value={selectedFilter ? (filters[selectedFilter] || '') : ''}
             onChange={handleFilterValueChange}
             label={filterOptions.find(opt => opt.key === selectedFilter)?.name ? 
