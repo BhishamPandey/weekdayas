4e368f662044495ff2819f1e23f976c0168f9ccb
BhishamPandey <pandeybhishm2@gmail.com>
2024-05-05 23:24:09 +0530

    added filter component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Filters from './jobComponents/filters';
+import { useState } from 'react';
 function App() {
+ // create filters so that any changes in filtering Text  we can pass them as props and according state mangement and filtering is done by reducers
+  const [filters, setFilters] = useState({ location: '' ,jobRole:'',minExp:'',minJdSalary:'',workType:'',companyName:''});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Bhism pandey
-        </a>
-      </header>
+      <Filters filters={filters} setFilters={setFilters} />
     </div>
   );
 }
