35f594751609ab6fd32dea27b99f7faad95056cc
BhishamPandey <pandeybhishm2@gmail.com>
2024-05-05 23:13:56 +0530

    App.js changed

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Bhism pandey
         </a>
       </header>
     </div>
