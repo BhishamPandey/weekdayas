1e741100b867758afc33a5df369403a970ff452f
BhishamPandey <pandeybhishm2@gmail.com>
2024-05-06 17:52:51 +0530

    changes to filters

--- src/shared/filtersUi/filterInput.jsx
+++ src/shared/filtersUi/filterInput.jsx
@@ -6,26 +6,26 @@ const FilterInput = ({ selectedFilter, filters, handleFilterValueChange, handleW
     <Box sx={{ mb: 2 }}> 
       {selectedFilter === 'workType' ? (
         <FormControl component="fieldset"> 
-          <FormLabel component="legend">Work Type</FormLabel> {/* Label for work type selection */}
+          <FormLabel component="legend">Work Type</FormLabel>
           <RadioGroup
-            row // Arrange radio buttons horizontally
-            name="workType" // Name attribute for radio buttons
+            row
+            name="workType"
             value={filters.workType || ''} 
             onChange={handleWorkTypeChange} 
           >
-            <FormControlLabel value="remote" control={<Radio />} label="Remote" /> {/* Remote option */}
-            <FormControlLabel value="onsite" control={<Radio />} label="On-site" /> {/* On-site option */}
+            <FormControlLabel value="remote" control={<Radio />} label="Remote" />
+            <FormControlLabel value="onsite" control={<Radio />} label="On-site" />
           </RadioGroup>
         </FormControl>
       ) : (
         <TextField
-          fullWidth // Occupy full width of container
-          type="text" // Text input field
-          name={selectedFilter} // Name attribute for text field
-          value={filters[selectedFilter] || ''} // Set initial value based on filter state
-          onChange={handleFilterValueChange} // Function to handle text input changes
-          label={`Enter ${filterOptions.find(opt => opt.key === selectedFilter)?.name}`} // Dynamic label based on filter option name
-          variant="outlined" // Outlined text field style
+          fullWidth
+          type="text"
+          name={selectedFilter}
+          value={filters[selectedFilter] || ''}
+          onChange={handleFilterValueChange}
+          label={`Enter ${filterOptions.find(opt => opt.key === selectedFilter)?.name}`}
+          variant="outlined"
         />
       )}
     </Box>
