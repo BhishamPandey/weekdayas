59a0c49313409adf7f452955815dabe139a00f0e
BhishamPandey <pandeybhishm2@gmail.com>
2024-05-06 19:12:40 +0530

    index changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
